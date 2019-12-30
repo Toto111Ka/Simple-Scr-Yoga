d4e1af37fe9d74389fd2c91bd6ae99b9dc0a4161
Igor <faucter@gmail.com>
2019-12-30 16:13:17 +0300

    Fix bugs
    + Days + Month

--- js/script.js
+++ js/script.js
@@ -39,16 +39,20 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer
 
-    let deadLine = '2020-10-21';
+    let deadLine = '2019-12-31';
 
     function getTimeRemaining (endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
         seconds = Math.floor((t/1000) % 60), //получение целых чисел вместо дробных
         minutes = Math.floor((t/1000/60) % 60),
-        hours = Math.floor((t/(1000*60*60)));
+        hours = Math.floor((t/(1000*60*60)) % 24),
+        days = Math.floor((t/(1000*3600*24)) % 30),
+        month = Math.floor(t/(1000*3600*24*30));
 
         return {
             'total' : t,
+            'month' : month,
+            'days'  : days,
             'hours' : hours,
             'minutes' : minutes,
             'seconds' : seconds
@@ -57,19 +61,28 @@ window.addEventListener('DOMContentLoaded', function() {
 
     function stClock(id, endtime) {
         let timer = document.getElementById(id),
+            month = timer.querySelector('.month'),
+            days = timer.querySelector('.days'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds');
+            seconds = timer.querySelector('.seconds'),
             timeInterval = setInterval(updateClock, 1000);
 
         function updateClock () {
             let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
-
-            if (t.total <=0) {
+            month.textContent = (t.month < 1) ? " ": t.month + "M";
+            days.textContent = (t.days < 1) ? " ": t.days + "D";
+            hours.textContent = (t.hours >= 10) ? t.hours : "0" + t.hours;
+            minutes.textContent = (t.minutes >= 10) ? t.minutes : "0" + t.minutes;
+            seconds.textContent = (t.seconds >= 10) ? t.seconds : "0" + t.seconds;
+            
+            if (t.total <= 0) {
                 clearInterval(timeInterval);
+                month.textContent = ' ';
+                days.textContent = ' ';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             }
         }
     }
