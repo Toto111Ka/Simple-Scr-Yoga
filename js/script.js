0c4447ba3f7fac9432410e562021bf0fdb63c940
Igor <faucter@gmail.com>
2020-02-27 14:09:58 +0300

    3.0 F

--- js/script.js
+++ js/script.js
@@ -116,31 +116,81 @@ window.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // From
+        // From
 
     let message = {
         loading: 'Загрузка...',
         success: 'Спасибо скоро мы свяжимся с вами!',
-        failure: 'чтото пошло не так'
+        failure: 'Чтото пошло не так'
     };
 
     let form = document.querySelector('.main-form'),
-        input = form.getElementsByTagName('input'),
+        contactForm = document.getElementById('form'),
+        input = document.getElementsByTagName('input'),
         statusMassage = document.createElement('div');
-
-        statusMassage.classlist.add('status');
+        statusMassage.classList.add('status');
     
     form.addEventListener('submit', function(event){
         event.preventDefault();
         form.appendChild(statusMassage);
 
-        let request = new XMLGttpRequest();
+        let request = new XMLHttpRequest();
         request.open('POST', 'server.php');
-        request.setRequestHeader ('Content-Type', 'application/x-ww-from-urkencoded');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        //request.setRequestHeader ('Content-type', 'application/json; charset=utf-8'); // JSON form
 
+        let formData = new FormData(form);
+
+        // let obj = {};   // преобразование данных в читаемый объект
+        // formData.forEach(function(value, key) {
+        //     obj[key] = value;
+        // });
+        // let json = JSON.stringify(obj);
+        // request.send(json);
+
+        request.send(formData);
+
+        request.addEventListener('readystatechange', function(){
+            if (request.readyState < 4) {
+                statusMassage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status ==200) {
+                statusMassage.innerHTML = message.success;
+            } else {
+                statusMassage.innerHTML = message.failure;
+            }
+        });
         
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
     });
 
+    contactForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        contactForm.appendChild(statusMassage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        let contactFormData = new FormData(contactForm);
+        request.send(contactFormData);
+
+        request.addEventListener('readystatechange', function(){
+            if (request.readyState < 4) {
+                statusMassage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status ==200) {
+                statusMassage.innerHTML = message.success;
+            } else {
+                statusMassage.innerHTML = message.failure;
+            }
+            statusMassage.style.color = 'gray';
+        });
+        
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
 });
 
 
